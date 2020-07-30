390e2a4702214f43a6199fa0b7248bd99772f2fb
VectorShadow <ncbrayshaw@hotmail.com>
2020-07-30 10:23:17 -0500

    Added coordinates, definitions, and basic events.

--- src/gamestate/TestGameZoneGenerator.java
+++ src/gamestate/TestGameZoneGenerator.java
@@ -1,12 +1,14 @@
 package gamestate;
 
+import gamestate.terrain.TerrainTile;
+
 public class TestGameZoneGenerator extends GameZoneGenerator {
     @Override
     public GameZone generate() {
         GameZone gz = new GameZone(32, 32);
         for (int i = 0; i < gz.ROWS; ++i) {
             for (int j = 0; j < gz.COLUMNS; ++j) {
-                gz.terrainMap[i][j] = new TerrainTile(0);
+                gz.terrain[i][j] = new TerrainTile(0);
             }
         }
         return gz;
