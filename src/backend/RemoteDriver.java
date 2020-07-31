96241b492a1acd1e8fe6c0cead4b08a879a5582f
VectorShadow <ncbrayshaw@hotmail.com>
2020-07-31 16:18:46 -0500

    Logger: Calling log now adds a line break.
    ShadowComm:
    Added isEncrypted().
    
    dydx2:
    UserAccount infrastructure added, including file management, account related InstructionDatum classes, and DataHandler support for those instructions.
    
    ViridianDrive:
    Updated Local and RemoteDrivers to test account infrastructure updates in dydx2.

--- src/backend/RemoteDriver.java
+++ src/backend/RemoteDriver.java
@@ -1,11 +1,18 @@
 package backend;
 
+import definitions.DefinitionsManager;
+import definitions.ViridianDriveGameZoneGenerator;
+import definitions.ViridianDriveTerrainLookup;
 import main.LogHub;
 
 import java.io.IOException;
 
 public class RemoteDriver {
     public static void main(String[] args) {
+        DefinitionsManager.loadDefinitions(
+                new ViridianDriveGameZoneGenerator(),
+                new ViridianDriveTerrainLookup()
+        );
         try {
             EngineManager.startRemoteEngine();
         } catch (IOException e) {
--- src/frontend/LocalDriver.java
+++ src/frontend/LocalDriver.java
@@ -5,15 +5,47 @@ import definitions.DefinitionsManager;
 import definitions.ViridianDriveGameZoneGenerator;
 import definitions.ViridianDriveTerrainLookup;
 import link.DataLink;
+import link.instructions.AccountCreationRequestInstructionDatum;
+import link.instructions.LoginRequestInstructionDatum;
 import main.LiveLog;
+
+import java.io.IOException;
 
 public class LocalDriver {
     public static void main(String[] args) {
         LiveLog.setConsoleOutLevel(LiveLog.LogEntryPriority.INFO);
-        DefinitionsManager.loadDefinitions(new ViridianDriveGameZoneGenerator(), new ViridianDriveTerrainLookup());
+        DefinitionsManager.loadDefinitions(
+                new ViridianDriveGameZoneGenerator(),
+                new ViridianDriveTerrainLookup()
+        );
         GuiManager.launchGui();
         GuiManager.getGui().update();
         //todo - hack! start a local engine for now
-        DataLink frontend = EngineManager.startLocalEngine();
+        final boolean TEST_LOCAL = true;
+        DataLink frontend = null;
+        try {
+            if (TEST_LOCAL)
+                frontend = EngineManager.startLocalEngine();
+            else
+                frontend = EngineManager.connectToRemoteEngine();
+        } catch (IOException e) {
+            System.out.println("Exception on engine initialization:\n" + e);
+        }
+        try {
+            Thread.sleep(500);
+            //test login - account does not exist
+            frontend.transmit(new LoginRequestInstructionDatum("user", "pass"));
+            Thread.sleep(500);
+            //test creation
+            frontend.transmit(new AccountCreationRequestInstructionDatum("user", "pass"));
+            Thread.sleep(500);
+            //test login - incorrect password
+            frontend.transmit(new LoginRequestInstructionDatum("user", "wrongpass"));
+            Thread.sleep(500);
+            //test login - success
+            frontend.transmit(new LoginRequestInstructionDatum("user", "pass"));
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted exception during instruction testing:\n" + e);
+        }
     }
 }
