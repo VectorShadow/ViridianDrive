fd58bbc9ffd4b46c3238fc113f75234b0c6ead71
VectorShadow <ncbrayshaw@hotmail.com>
2020-07-31 21:51:25 -0500

    Logger: Provided public access to the log directory path.
    ShadowComm:
    Removed test package.
    
    dydx2:
    Improved engine logging.
    Provided public access to the user directory path.
    
    ViridianStorm:
    Added ArgumentHandler class to parse program execution arguments.
    RemoteDriver now always calls for argument parsing.

--- src/backend/RemoteDriver.java
+++ src/backend/RemoteDriver.java
@@ -4,11 +4,13 @@ import definitions.DefinitionsManager;
 import definitions.ViridianDriveGameZoneGenerator;
 import definitions.ViridianDriveTerrainLookup;
 import main.LogHub;
+import util.ArgumentHandler;
 
 import java.io.IOException;
 
 public class RemoteDriver {
     public static void main(String[] args) {
+        ArgumentHandler.handle(args);
         DefinitionsManager.loadDefinitions(
                 new ViridianDriveGameZoneGenerator(),
                 new ViridianDriveTerrainLookup()
---
+++ src/util/ArgumentHandler.java
@@ -0,0 +1,57 @@
+package util;
+
+import main.LiveLog;
+import main.LogHub;
+import user.UserAccountManager;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class ArgumentHandler {
+    public static void main(String[] args) {
+        handle(args);
+    }
+    public static void handle(String[] args) {
+        if (args.length == 0) return;
+        switch (args[0]) {
+            case "clean":
+                for (int i = 1; i < args.length; ++i) {
+                    String arg = args[i];
+                    switch (arg) {
+                        case "log":
+                            LiveLog.stop(); //terminate any active logging
+                            removeRecursively(LogHub.getDirectoryPath());
+                            break;
+                        case "usr":
+                            removeRecursively(UserAccountManager.getDirectoryPath());
+                            break;
+                            //todo - others?
+                    }
+                }
+                break;
+                //todo - others?
+        }
+    }
+    private static void removeRecursively(Path directoryPath) {
+        if (Files.exists(directoryPath)) {
+            try {
+                Files.walkFileTree(directoryPath, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        Files.delete(file);
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            } catch (IOException e) {
+                LogHub.logNonFatalError("Error while removing directory at path " + directoryPath, e);
+            }
+        }
+    }
+}
